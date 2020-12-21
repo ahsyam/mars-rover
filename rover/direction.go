ca4a7398b3ff5687e0accb1ed8a9007601d80d3d
Ahmed Syam <engsyam@gmail.com>
2020-12-21 20:00:19 +0100

    writing refactor code to make tests pass

--- rover/direction.go
+++ rover/direction.go
@@ -23,54 +23,70 @@ type West struct{}
 type South struct{}
 
 func (n North) forward(p *position) *position {
+	p.Y++
 	return p
 }
 func (n North) backward(p *position) *position {
+	p.Y--
 	return p
 }
 func (n North) left(p *position) *position {
+	p.Direction = WEST
 	return p
 }
 func (n North) right(p *position) *position {
+	p.Direction = EAST
 	return p
 }
 
 func (e East) forward(p *position) *position {
+	p.X++
 	return p
 }
 
 func (e East) backward(p *position) *position {
+	p.X--
 	return p
 }
 func (e East) left(p *position) *position {
+	p.Direction = NORTH
 	return p
 }
 func (e East) right(p *position) *position {
+	p.Direction = SOUTH
 	return p
 }
 
 func (s South) forward(p *position) *position {
+	p.Y--
 	return p
 }
 func (s South) backward(p *position) *position {
+	p.Y++
 	return p
 }
 func (s South) left(p *position) *position {
+	p.Direction = EAST
 	return p
 }
 func (s South) right(p *position) *position {
+	p.Direction = WEST
 	return p
 }
 
 func (w West) forward(p *position) *position {
+	p.X--
 	return p
 }
 func (w West) backward(p *position) *position {
+	p.X++
 	return p
 }
 func (w West) left(p *position) *position {
+	p.Direction = SOUTH
 	return p
 }
 func (w West) right(p *position) *position {
+	p.Direction = NORTH
 	return p
 }
